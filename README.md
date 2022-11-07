1d801f7437de229ad74c7dbf45a859f4d9587e58
unknown <shriyaellur9@gmail.com>
2022-11-07 16:32:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Face mask detection
+
+•	It is a project which deals with Real time Face mask detection using local webcam.
+•	It deals with loading the saved model to make prediction, where model has been trained on images with and without mask
+•	It also uses haarcascade-frontalface classifier along  with the concept of Bounding boxes(YOLO) around the face and gives appropriate output.
---
+++ training.py
@@ -0,0 +1,54 @@
+import numpy as np
+import keras
+import keras.backend as k
+from keras.layers import Conv2D,MaxPooling2D,SpatialDropout2D,Flatten,Dropout,Dense
+from keras.models import Sequential,load_model
+from keras.optimizers import adam
+from keras.preprocessing import image
+import cv2
+import datetime
+
+
+
+model=Sequential()
+model.add(Conv2D(32,(3,3),activation='relu',input_shape=(150,150,3)))
+model.add(MaxPooling2D() )
+model.add(Conv2D(32,(3,3),activation='relu'))
+model.add(MaxPooling2D() )
+model.add(Conv2D(32,(3,3),activation='relu'))
+model.add(MaxPooling2D() )
+model.add(Flatten())
+model.add(Dense(100,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        'train',
+        target_size=(150,150),
+        batch_size=16 ,
+        class_mode='binary')
+test_set = test_datagen.flow_from_directory(
+        'test',
+        target_size=(150,150),
+        batch_size=16,
+        class_mode='binary')
+model_saved=model.fit_generator(
+        training_set,
+        epochs=10,
+        validation_data=test_set,
+        )
+model.save('mymodel.h5',model_saved)
+#To test for individual images
+test_image=image.load_img('test/without_mask/30.jpg',target_size=(150,150,3))
+
+test_image
+test_image=image.img_to_array(test_image)
+test_image=np.expand_dims(test_image,axis=0)
+type(mymodel.predict_classes(test_image)[0][0])
+
